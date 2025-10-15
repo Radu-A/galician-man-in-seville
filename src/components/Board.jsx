9cf6595283f9e675b6113310a650a78df732faf4
Radu-A <elcastravete@gmail.com>
2025-10-15 11:03:49 +0200

    added/data.js

---
+++ src/components/Board.jsx
@@ -0,0 +1,61 @@
+export default function Board({ posts }) {
+  return (
+    <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 dark:border-gray-700">
+      {posts.map((post) => (
+        <article
+          key={post.id}
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 max-w-xl"
+        >
+          <div>
+            <img src={post.img} alt="" className="h-50 rounded-xl" />
+          </div>
+          <div className="flex flex-col items-start justify-between">
+            <div className="flex items-center gap-x-4 text-xs">
+              <time
+                dateTime={post.datetime}
+                className="text-gray-500 dark:text-gray-400"
+              >
+                {post.date}
+              </time>
+              <a
+                href={post.category.href}
+                className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100 dark:bg-gray-800/60 dark:text-gray-300 dark:hover:bg-gray-800"
+              >
+                {post.category.title}
+              </a>
+            </div>
+            <div className="group relative grow">
+              <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600 dark:text-white dark:group-hover:text-gray-300">
+                <a href={post.href}>
+                  <span className="absolute inset-0" />
+                  {post.title}
+                </a>
+              </h3>
+              <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600 dark:text-gray-400">
+                {post.description}
+              </p>
+            </div>
+            <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
+              {/* <img
+                            alt=""
+                            src={post.author.imageUrl}
+                            className="size-10 rounded-full bg-gray-50 dark:bg-gray-800"
+                          /> */}
+              <div className="text-sm/6">
+                <p className="font-semibold text-gray-900 dark:text-white">
+                  <a href={post.author.href}>
+                    <span className="absolute inset-0" />
+                    {post.author.name}
+                  </a>
+                </p>
+                <p className="text-gray-600 dark:text-gray-400">
+                  {post.author.role}
+                </p>
+              </div>
+            </div>
+          </div>
+        </article>
+      ))}
+    </div>
+  );
+}
--- src/pages/Events.jsx
+++ src/pages/Events.jsx
@@ -1,59 +1,6 @@
-const posts = [
-  {
-    id: 1,
-    title: "Jam Session Pumarejo",
-    href: "#",
-    img: "public/images/event-pumarejo.jpeg",
-    description:
-      "Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.",
-    date: "Oct 3, 2025",
-    datetime: "2025-10-03",
-    category: { title: "Concierto", href: "#" },
-    author: {
-      name: "Nazmar",
-      role: "Casa Pumarejo",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  {
-    id: 2,
-    title: "Electrolunch Sevilla",
-    href: "#",
-    img: "public/images/event-electrolunch.jpg",
-    description:
-      "Optio cum necessitatibus dolor voluptatum provident commodi et. Qui aperiam fugiat nemo cumque.",
-    date: "Mar 10, 2020",
-    datetime: "2020-03-10",
-    category: { title: "Sales", href: "#" },
-    author: {
-      name: "Varios",
-      role: "CAAC",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-  {
-    id: 3,
-    title: "Improve your customer experience",
-    href: "#",
-    img: "public/images/event-electrolunch.jpg",
-    description:
-      "Cupiditate maiores ullam eveniet adipisci in doloribus nulla minus. Voluptas iusto libero adipisci rem et corporis. Nostrud sint anim sunt aliqua. Nulla eu labore irure incididunt velit cillum quis magna dolore.",
-    date: "Feb 12, 2020",
-    datetime: "2020-02-12",
-    category: { title: "Business", href: "#" },
-    author: {
-      name: "Tom Cook",
-      role: "Director of Product",
-      href: "#",
-      imageUrl:
-        "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-    },
-  },
-];
+import { eventsArray } from "../utils/data";
+
+import Board from "../components/board";
 
 export default function Events() {
   return (
@@ -68,63 +15,7 @@ export default function Events() {
             impresiones, lo más llamativo, lo más mierder...
           </p>
         </div>
-        <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 dark:border-gray-700">
-          {posts.map((post) => (
-            <article
-              key={post.id}
-              className="grid grid-cols-2 lg:grid-cols-1 max-w-xl"
-            >
-              <div>
-                <img src={post.img} alt="" className="h-50 rounded-xl" />
-              </div>
-              <div className="flex flex-col items-start justify-between">
-                <div className="flex items-center gap-x-4 text-xs">
-                  <time
-                    dateTime={post.datetime}
-                    className="text-gray-500 dark:text-gray-400"
-                  >
-                    {post.date}
-                  </time>
-                  <a
-                    href={post.category.href}
-                    className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100 dark:bg-gray-800/60 dark:text-gray-300 dark:hover:bg-gray-800"
-                  >
-                    {post.category.title}
-                  </a>
-                </div>
-                <div className="group relative grow">
-                  <h3 className="mt-3 text-lg/6 font-semibold text-gray-900 group-hover:text-gray-600 dark:text-white dark:group-hover:text-gray-300">
-                    <a href={post.href}>
-                      <span className="absolute inset-0" />
-                      {post.title}
-                    </a>
-                  </h3>
-                  <p className="mt-5 line-clamp-3 text-sm/6 text-gray-600 dark:text-gray-400">
-                    {post.description}
-                  </p>
-                </div>
-                <div className="relative mt-8 flex items-center gap-x-4 justify-self-end">
-                  {/* <img
-                    alt=""
-                    src={post.author.imageUrl}
-                    className="size-10 rounded-full bg-gray-50 dark:bg-gray-800"
-                  /> */}
-                  <div className="text-sm/6">
-                    <p className="font-semibold text-gray-900 dark:text-white">
-                      <a href={post.author.href}>
-                        <span className="absolute inset-0" />
-                        {post.author.name}
-                      </a>
-                    </p>
-                    <p className="text-gray-600 dark:text-gray-400">
-                      {post.author.role}
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </article>
-          ))}
-        </div>
+        <Board posts={eventsArray}></Board>
       </div>
     </div>
   );
