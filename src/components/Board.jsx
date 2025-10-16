243249a4b50f44f0bea39e2e1c7820ce6be1fe00
Radu-A <elcastravete@gmail.com>
2025-10-16 21:10:05 +0200

    added/style

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,6 +1,6 @@
 export default function Board({ posts }) {
   return (
-    <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 dark:border-gray-700">
+    <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 dark:border-gray-700">
       {posts.map((post) => (
         <article
           key={post.id}
--- src/pages/AlbumView.jsx
+++ src/pages/AlbumView.jsx
@@ -3,20 +3,19 @@ import Board from "../components/Board";
 
 export default function AlbumView() {
   return (
-    <div className="bg-white py-24 sm:py-32 dark:bg-gray-900">
+    <section className="bg-white pt-6 sm:pt-12 dark:bg-gray-900">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl dark:text-white">
-            Un Gallego Por Sevilla
+            Mis hallazgos por Sevilla
           </h2>
           <p className="mt-2 text-lg/8 text-gray-600 dark:text-gray-300">
-            Encontrándome gente, cosas que hacer y lugares interesantes,
-            mientras escapo del aburrimiento y practico desarrollo para ser
-            programador.
+            Una recopilación visual de lugares, detalles y momentos que me
+            inspiran a seguir creando. Cada álbum es una parte del camino.
           </p>
         </div>
         <Board posts={placesArray}></Board>
       </div>
-    </div>
+    </section>
   );
 }
